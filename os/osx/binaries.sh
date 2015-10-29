b3229a67dfdfbf6a929f5b9e61a210bdec5689b8
John Kaczmarek <johnkacz@gmail.com>
2015-10-29 14:09:08 -0500

    (feature) Add ruby installation and default gems in configuration file

--- os/osx/binaries.sh
+++ os/osx/binaries.sh
@@ -15,9 +15,17 @@ source $config
 brew install ${binaries[@]}
 
 # Install the latest stable node version and the global npm packages
+# TODO - Check to see if nvm is installed first
 nvm install stable
 nvm alias default stable
 npm install -g ${globalNpmPackages[@]}
+
+# Install the latest stable ruby version and the global ruby gems
+# TODO - Check to see if rbenv is installed first
+rbenv init -
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+gem update --system
+gem install -g ${globalRubyGems[@]}
 
 # Install oh-my-zsh
 curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
