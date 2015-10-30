f653bc8859109247c8f27e6071bf1f9850c94f5c
John Kaczmarek <johnkacz@gmail.com>
2015-10-30 14:54:33 -0500

    Install Bash 4 and tweak profile

--- os/osx/profile.sh
+++ os/osx/profile.sh
@@ -30,8 +30,8 @@ fi
 
 # Lunchy tab completion
 LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
-if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
-  . $LUNCHY_DIR/lunchy-completion.bash
+if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
+  . $LUNCHY_DIR/lunchy-completion.zsh
 fi
 
 ###########
